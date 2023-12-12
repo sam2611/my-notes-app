3cb37b0314e3954bcfb0351bec194333ccac6bfa
Sakshi Mishra <84084388+sam2611@users.noreply.github.com>
2023-12-12 20:21:31 +0530

    frontend

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+
+import Header from './components/partials/Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./components/Home.jsx"
+import Register from "./components/Register.jsx"
+import Login from "./components/Login.jsx"
+import { useState } from 'react';
+
+function App() {
+
+  const info = localStorage.getItem('user');
+  const [user, setUser]= useState(JSON.parse(info));
+  return (
+    <>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/login" element={<Login user={user} setUser={setUser} />}></Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
