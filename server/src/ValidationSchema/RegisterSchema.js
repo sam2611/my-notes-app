52db9b0857467d156c40538c17a9085fa091f041
Sakshi Mishra <84084388+sam2611@users.noreply.github.com>
2023-12-12 21:35:44 +0530

    backend

---
+++ server/src/ValidationSchema/RegisterSchema.js
@@ -0,0 +1,14 @@
+import {check} from "express-validator";
+
+export const RegisterSchema=[
+    check('name').trim().isAlpha()
+    .withMessage("Name Should be alphabets only"),
+
+    check('username', 'username is required').exists()
+    .isAlphanumeric().withMessage('username should be alphanumeric character only')
+    .trim().isLength({min:6,max:32}),
+
+    check('password','Password is required').isLength({min:6,max:18}).trim(),
+
+    check('email', 'email is required').exists().isEmail(),
+]
---
+++ server/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const DB_CONNECT="mongodb+srv://sakshimishra2611:root123@cluster0.qsih2of.mongodb.net/todo?retryWrites=true&w=majority"
+
+export const JWT_TOKEN_SECRET="anything"
+
+export const StatusCode={
+    SUCCESS: 200,
+    VALIDATION_ERROR:201,
+    UNPROCESSABLE_ENTITY:202,
+    AUTH_ERROR: 203
+}
