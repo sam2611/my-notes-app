0a52cfd09e4f18dfb212fe21a5e02972573ccf5e
sam2611 <sakshimishra2611@gmail.com>
2023-12-12 23:11:49 +0530

    backend

---
+++ client/src/components/partials/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">TODO App</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor04" aria-controls="navbarColor04" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarColor04">
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <Link className="nav-link active" to="/">Home
+            <span className="visually-hidden">(current)</span>
+            </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="login">Login</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">Register</Link>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Logout</a>
+        </li>
+        
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-sm-2" type="search" placeholder="Search"/>
+        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Header
