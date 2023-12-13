e82a8ffa98a43d865262d8bec63956245ac5c772
sam2611 <sakshimishra2611@gmail.com>
2023-12-13 19:17:34 +0530

    registration page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,9 +2,53 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>
-      register
+    <>
+    <div className="container">
+      <div className="row justify-content-md-center mt-4">
+        <div className="col-lg-5 card border-primary mb-3">
+            <div className="card-header h4 text-canter">
+              Register An Account
+            </div>
+            <div className="card-body">
+              <div className="form-group">
+                <label className="col-form-label mt-4">
+                  Username
+                </label>
+                <input type="text"
+                 className="form-control" 
+                 placeholder="Username"/>
+
+
+              </div>
+
+              <div className="form-group">
+                <label className="col-form-label mt-4">
+                  Email
+                </label>
+                <input type="email"
+                 className="form-control" 
+                 placeholder="Email"/>
+              </div>
+
+              <div className="form-group">
+                <label className="col-form-label mt-4">
+                  Password
+                </label>
+                <input type="password"
+                 className="form-control" 
+                 placeholder="Enter your Password"/>
+              </div>
+              <div className="row justify-content-md-center form-group mt-4">
+                <button type="submit" className='col-sm-6 btn btn-outline-secondary center'>
+                  Register Now
+                </button>
+
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
 
