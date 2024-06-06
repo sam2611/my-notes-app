4bc33e888a541a0778b711af27047609f28c6a58
Sakshi Mishra <sakshimishra2611@gmail.com>
2024-06-07 00:27:40 +0530

    deploying

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL='https://my-notes-app-cfqp.onrender.com' // for deploying purpose
-export const API_URL='http://localhost:8000'
+export const API_URL='https://my-notes-app-uuyp.onrender.com' // for deploying purpose
+// export const API_URL='http://localhost:8000'
