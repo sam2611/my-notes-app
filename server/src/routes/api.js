e5d26db3e1ca90c0bf8adc3d55070d1bb6a34107
Sakshi Mishra <sakshimishra2611@gmail.com>
2024-07-19 08:32:09 +0530

    api

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,32 +1,25 @@
-import express from "express";
-import Register from "../controllers/Register.controller.js";
-import { RegisterSchema } from "../ValidationSchema/RegisterSchema.js";
-import { LoginSchema } from "../ValidationSchema/LoginSchema.js";
-import Login from "../controllers/Login.controller.js";
-import { createTodo } from "../controllers/Todo.controller.js";
+import express from 'express';
+import Register from '../controllers/Register.controller.js';
+import { RegisterSchema } from '../ValidationSchema/RegisterSchema.js';
+import { LoginSchema } from '../ValidationSchema/LoginSchema.js';
+import Login from '../controllers/Login.controller.js';
+import { createTodo } from '../controllers/Todo.controller.js';
 import { check } from 'express-validator';
-import { GetTodos } from "../controllers/TodoList.controller.js";
-import { MarkTodo } from "../controllers/MarkTodo.controller.js";
-import { RemoveTodo } from "../controllers/RemoveTodo.controller.js";
+import { GetTodos } from '../controllers/TodoList.controller.js';
+import { MarkTodo } from '../controllers/MarkTodo.controller.js';
+import { RemoveTodo } from '../controllers/RemoveTodo.controller.js';
 
-const apiRoute= express.Router();
+const apiRoute = express.Router();
 export const apiProtected = express.Router();
 
-apiRoute.post("/register", RegisterSchema ,Register);
-apiRoute.post("/login",LoginSchema,Login)
+apiRoute.post('/register', RegisterSchema, Register);
+apiRoute.post('/login', LoginSchema, Login);
 
-//protected routes
+// protected routes
+apiProtected.post('/createTodo', [check('desc', 'Todo desc is required').exists()], createTodo);
+apiProtected.post('/markTodo', [check('todo_id', 'Todo id is required').exists()], MarkTodo);
+apiProtected.post('/deleteTodo', [check('todo_id', 'Todo id is required').exists()], RemoveTodo);
 
-apiProtected.post("/createTodo",[check("desc","Todo desc is required").exists()],
-createTodo);
-
-apiProtected.post("/markTodo",[check("todo_id","Todo id is required").exists()],
-MarkTodo);
-
-apiProtected.post("/deleteTodo",[check("todo_id","Todo id is required").exists()],
-RemoveTodo);
-
-apiProtected.get("/todolist",
-GetTodos);
+apiProtected.get('/todolist', GetTodos);
 
 export default apiRoute;
